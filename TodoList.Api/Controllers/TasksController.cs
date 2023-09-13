38a70b3adc2e83f6a9fc3e243740235c68054ecf
Hieu Trinh <hieu.trinh@bliksund.com>
2023-09-13 16:18:39 +0700

    CRUD todo list

--- TodoList.Api/Controllers/TasksController.cs
+++ TodoList.Api/Controllers/TasksController.cs
@@ -20,5 +20,47 @@ namespace TodoList.Api.Controllers
             var task = await _taskRepository.GetTaskList();
             return Ok(task);
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Entities.Task task)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var tasks = await _taskRepository.Create(task);
+            return CreatedAtAction(nameof(GetById), new { id = task.Id }, tasks);
+        }
+
+        [HttpPut]
+        [Route("{id}")]
+        public async Task<IActionResult> Update(Guid id, Entities.Task task)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var taskFromDb = await _taskRepository.GetById(id);
+
+            if (taskFromDb == null)
+            {
+                return NotFound($"{id} is not found");
+            }
+
+            taskFromDb.Name = task.Name;
+            var tasks = await _taskRepository.Update(task);
+
+            return Ok(tasks);
+        }
+
+
+        //api/tasks/xxxx
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<IActionResult> GetById([FromRoute] Guid id)
+        {
+            var task = await _taskRepository.GetById(id);
+            if (task == null) return NotFound($"{id} is not found");
+            return Ok(task);
+        }
+
     }
 }
